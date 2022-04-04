ab87b97c809a95474ba70f906a2aef6d1e8b3101
Anna <iostream.lib@gmail.com>
2022-04-04 06:15:39 +0300

    Start of lw4. Added blocks.

---
+++ LW4/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="blocks.css">
+    <script>
+        $(document).ready(function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
+</head>
+
+<body>
+<div class="block1">
+    <h1>ЛОГОТИП</h1>
+</div>
+
+<div class="block2">
+    <h1>Название</h1>
+</div>
+
+<div class="block3">
+    <h1>Регистрация/вход</h1>
+</div>
+
+<div class="block4">
+    <h1>Панель навигации</h1>
+</div>
+
+<div class="block5">
+    <h1>КОНТЕНТ</h1>
+</div>
+
+<div class="block6">
+    <h1>Реклама + новости</h1>
+</div>
+
+<div class="block7">
+    <h1 class="block7">Авторская работа студентки Черновой А.А.</h1>
+</div>
+</body>
+</html>
